0e09c393fccb62a5088524133604b3d08e6136c7
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-30 13:25:30 -0500

    Add custom instance methods #previous_days_food and #two_days_ago_food to Food and Symptom

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,4 +2,16 @@ class Food < ActiveRecord::Base
   has_many :days
   has_many :symptoms, through: :days
   validates_presence_of :name, :serving
+
+  def nested_array_of_each_previous_days_symptoms
+    self.days.each do |day|
+      day.previous_day.symptoms
+    end
+  end
+
+  def nested_array_of_each_two_days_ago_symptoms
+    self.days.each do |day|
+      day.two_days_ago.symptoms
+    end
+  end
 end
--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -2,4 +2,16 @@ class Symptom < ActiveRecord::Base
   has_many :days
   has_many :foods, through: :days
   validates_presence_of :type, :frequency
+
+  def nested_array_of_each_previous_days_foods
+    self.days.each do |day|
+      day.previous_day.foods
+    end
+  end
+
+  def nested_array_of_each_two_days_ago_foods
+    self.days.each do |day|
+      day.two_days_ago.foods
+    end
+  end
 end
