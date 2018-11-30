d349c543b00d1780d5c0b7f8d42e2426f7773a57
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-30 10:43:54 -0500

    Add routes and nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "days#index"
+  get "signup", to "users#new"
+  resources :users do
+    resources :days
+    resources :food
+    resources :symptoms
+  end
 end
