0dade69250cbddfbff6318dcb6bfe6b204b7a1b0
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-30 13:11:38 -0500

    Add Day associations, validations and custom instance methods

---
+++ app/models/day.rb
@@ -0,0 +1,21 @@
+class Day < ActiveRecord::Base
+  belongs_to :user
+  has_many :foods
+  has_many :symptoms
+
+  def month_day_year
+	   self.created_at.strftime("%m-%d-%y")
+  end
+
+  def day_of_week
+	   self.created_at.strftime("%A")
+  end
+
+  def previous_day
+    Day.find_by(id: (self.id - 1)) || nil
+  end
+
+  def two_days_ago
+    Day.find_by(id: (self.id - 2)) || nil
+  end
+end
