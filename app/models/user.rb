55c1c65e23cdc04465c25408ebe42c97d94b9920
emanuelbierman <emanuel.bierman@gmail.com>
2018-11-30 13:06:54 -0500

    Add User associations and validations

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  has_secure_password
+  has_many: :days
+  has_many: :foods, through: :days
+  has_many: :symptoms, through: :days
+  validates_presence_of: :username, :password
+
+end
