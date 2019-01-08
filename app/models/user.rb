79cb3dc33bb79e9fe07a0d73470932de12a7d6bb
emanuelbierman <emanuel.bierman@gmail.com>
2019-01-08 11:16:39 -0500

    Create #food_names and #symptom_descriptions instance methods in User model that generate lists of previously entered names and descriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
   has_many :symptoms, through: :days
   validates_presence_of :username, :password
 
+  def food_names
+    self.foods.group(:name).distinct
+  end
+
+  def symptom_descriptions
+    self.symptoms.group(:description).distinct
+  end
+
 end
