55570f449db55e56bc52792bdb34031922066d91
emanuelbierman <emanuel.bierman@gmail.com>
2019-01-14 14:56:09 -0500

    Refactor DaysFOod#create to pass params to DaysFood model

--- app/controllers/days_foods_controller.rb
+++ app/controllers/days_foods_controller.rb
@@ -2,23 +2,12 @@ class DaysFoodsController < ApplicationController
 
   def create
     binding.pry
-    @food = DaysFood.create(days_food_params)
-    if @food.valid?
-      @food.save
-      @day = current_day(current_user.id)
-      comments = params[:days_food][:comments]
-      @days_food = DaysFood.create(day_id: @day.id, food_id: @food.id, comments: comments)
-      if @days_food.valid?
-        @days_food.save
-        # @day.update(comments: @days_food.comments) if @days_food.comments
-        redirect_to user_path(current_user), notice: "Your food has been entered."
-      elsif @days_food.errors.any?
-        flash[:alert] = []
-        redirect_to user_path(current_user), alert: @days_food.errors.full_messages
-      end
-    elsif @food.errors.any?
+    @days_food = DaysFood.new(days_food_params)
+    if @days_food.save
+      redirect_to user_path(current_user), notice: "Your food has been entered."
+    else
       flash[:alert] = []
-      redirect_to user_path(current_user), alert: @food.errors.full_messages
+      redirect_to user_path(current_user), alert: @days_food.errors.full_messages
     end
   end
 
