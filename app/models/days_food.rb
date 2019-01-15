d1d3c78eec27d5435c807359513b11e4f5e5a2cd
emanuelbierman <emanuel.bierman@gmail.com>
2019-01-15 12:25:09 -0500

    Build and associate objects in users#show controller. Add validation in users#show view to account for unsaved foods and symptoms from controller

--- app/models/days_food.rb
+++ app/models/days_food.rb
@@ -3,11 +3,16 @@ class DaysFood < ActiveRecord::Base
   belongs_to :food
   validates_presence_of :serving
 
-  def foods=(params)
+  def create_from_(params)
     day = Day.find_by(id: params[:day_id].to_i)
-    food = Food.find_or_create_by()
+    food = Food.find_or_create_by(id: params[:food_id].to_i)
+    food = Food.find_or_create_by(name: params[:food][:name])
     comments = params[:comments]
-    days_food = DaysFood.create(day_id: day.id, food_id: food.id, comments: comments)
+    days_food = DaysFood.create(day_id: day.id.to_s, food_id: food.id.to_s, comments: comments)
+  end
+
+  def food_attributes=(attributes)
+
   end
 
   # def self.create_food_from(params)
